68da56e9529717190f861b3a7a52438e243ed51b
Joel Rorseth <joelrorseth@gmail.com>
2017-07-06 16:16:54 -0400

    Clean up and add better css styling

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, OnInit}  from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
 import { HeroService }              from './hero.service';
+import { Hero }                     from './hero';
 
 import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: [ './hero-detail.component.css' ]
 })
 
 export class HeroDetailComponent implements OnInit {
-    @Input() hero: Hero;
 
     constructor(
         private heroService: HeroService,
